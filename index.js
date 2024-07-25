5c250a71ecaae4f8046c529fc25796bbda0630bd
dom <35171942+domluther@users.noreply.github.com>
2024-07-25 20:11:27 +0100

    docker port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   const person = { name: 'JK new', age: 38 };
   res.send(person);
 });
 
-app.listen(3000);
+app.listen(PORT);
